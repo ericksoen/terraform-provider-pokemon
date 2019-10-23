f4c769f5d1e3f916de8232aeb61f611701f0b583
ericksoen <michael.ericksen@gmail.com>
2019-10-22 23:06:06 -0500

    add method comments

--- config.go
+++ config.go
@@ -6,10 +6,14 @@ import (
 	poke "github.com/ericksoen/pokemon-go-client"
 )
 
+// Config defines the values needed to create
+// the PokemonGo Client
 type Config struct {
 	APIKey string
 }
 
+// Client creates an instance of the PokemonGo client
+// using the API Key from the provider config
 func (c *Config) Client() (*poke.Client, error) {
 	config := poke.Config{
 		APIKey: c.APIKey,
